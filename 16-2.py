86ab815b804a0e4ec33ddc9eb57febfc89407a30
Serge1003 <147014838+Serge1003@users.noreply.github.com>
2024-06-14 14:35:14 +0300

    Update 14:34_16-2.py

--- 16-2.py
+++ 16-2.py
@@ -1,10 +1,17 @@
 import re
 from typing import List, Any
 
-limit = int(input())
-i_dig = {i for i in range(1, (limit+1))}
+limit = int(input()) #искомое число
+
+i=0
+i_dig = {i for i in range(1, (99))} #исходный массив
 i_str = (",".join(map(str, i_dig)))
-# i_fin1 = re.findall({'high': 45, 'low': 0}', i_str)
-# r'([0-4][0-5])', i_str)
-i_fin: list[Any] = re.findall(r'(?>[0123456789].)', i_str)
+
+i=0
+i_limit_dig = {i for i in range(1, (limit+1))} #
+i_limit_str = (",".join(map(str, i_limit_dig)))
+# print(11, i_limit_dig) №Контролька
+# print(22, i_limit_str)
+i_fin = re.findall('|'.join(i_str), i_limit_str) # вывод от 1 до limit
+# print(33, *i_fin)
 print(*i_fin)
