84ed7a8a14f056c60460ef0b4c471fc03ba2e9a1
Serge1003 <147014838+Serge1003@users.noreply.github.com>
2024-06-14 13:56:41 +0300

    Update 16-2.py

--- 16-2.py
+++ 16-2.py
@@ -2,12 +2,9 @@ import re
 from typing import List, Any
 
 limit = int(input())
-i_dig = {i for i in range(1, 99)}
-print(i_dig)
+i_dig = {i for i in range(1, (limit+1))}
 i_str = (",".join(map(str, i_dig)))
-i_fin1 = re.findall(r'(g\d)', i_str)
 # i_fin1 = re.findall({'high': 45, 'low': 0}', i_str)
 # r'([0-4][0-5])', i_str)
-i_fin: list[Any] = re.findall(r'((?>[0123456789].))', i_str)
-print(*i_fin1)
+i_fin: list[Any] = re.findall(r'(?>[0123456789].)', i_str)
 print(*i_fin)
